70268db22426dfdc991012cb5a04c03d566072de
AndreyLucasPail <andreypail@hotmail.com>
2024-01-02 19:41:13 -0300

    update

--- lib/home/widget/product_tile.dart
+++ lib/home/widget/product_tile.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shoes_store/data/produtcs_data.dart';
+import 'package:shoes_store/products-screen/widgets/product_tab.dart';
 
 class ProductsTile extends StatelessWidget {
   const ProductsTile({super.key, this.category, this.brand,});
@@ -36,50 +37,57 @@ class ProductsTile extends StatelessWidget {
             
                 return Row(
                   children: [
-                    Card(
-                      elevation: 5,
-                      child: SizedBox(
-                        width: 180,
-                        height: 300,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [(productsData.images != null && productsData.images!.isNotEmpty ?
-                            Align(
-                              alignment: Alignment.topCenter,
-                              child: Container(
-                                padding: const EdgeInsets.all(8.0),
-                                height: 200,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: NetworkImage(productsData.images![0]),
-                                    fit: BoxFit.cover,
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => ProductTab(product: productsData))
+                        );
+                      },
+                      child: Card(
+                        elevation: 5,
+                        child: SizedBox(
+                          width: 180,
+                          height: 300,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [(productsData.images != null && productsData.images!.isNotEmpty ?
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  height: 200,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(productsData.images![0]),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
+                              ) : Container()
                               ),
-                            ) : Container()
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(4),
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  "${productsData.name}",
-                                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                )
+                              Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "${productsData.name}",
+                                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                  )
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  "R\$ ${productsData.price}",
-                                  style: const TextStyle(fontSize: 16),
-                                )
-                              ),
-                            ), 
-                          ],
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "R\$ ${productsData.price}",
+                                    style: const TextStyle(fontSize: 16),
+                                  )
+                                ),
+                              ), 
+                            ],
+                          ),
                         ),
                       ),
                     ),
